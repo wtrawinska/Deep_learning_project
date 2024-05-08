67ea18508677b0dad64f4bc0788322c46e5001ef
Paulina Kucharewicz <paulinakucharewicz2@gmail.com>
2024-05-08 22:04:52 +0200

    add celesta_output_metrics.py

---
+++ CELESTA/celesta_output_metrics.py
@@ -0,0 +1,44 @@
+import argparse
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import (average_precision_score, roc_auc_score, accuracy_score, precision_score,
+                             recall_score, f1_score, confusion_matrix)
+
+def evaluate_celesta_output(csv_file):
+    """
+    calculates metrics for celesta classifcation
+    """
+    df = pd.read_csv(csv_file)
+    y_pred = df['Final cell type']
+    y_true = df['cell_labels']
+    print(f"Prediction results for CELESTA:")
+    print(f"F1 macro score: {f1_score(y_true, y_pred, average='macro')}")
+    print(f"Accuracy score: {accuracy_score(y_true, y_pred)}")
+    matrix = confusion_matrix(y_true, y_pred)
+    cell_labels = y_true.unique()
+    cell_labels.sort()
+    matrix = confusion_matrix(y_true, y_pred, labels=cell_labels)
+    print("#############")
+    print(f"Per type accuracy:")
+    for i in [f'{cell_labels[i]}: {x}' for i, x in enumerate(matrix.diagonal() / matrix.sum(axis=1))]:
+        print(i)
+    print("#############")
+    print(f"Precision score: {precision_score(y_true, y_pred, average='macro')}")
+    print(f"Recall score: {recall_score(y_true, y_pred, average='macro')}")
+    y_score = OneHotEncoder().fit_transform(y_pred.to_numpy().reshape(-1, 1)).toarray()
+    print(f"ROC AUC per cell type score:")
+    for i in [f'{cell_labels[i]}: {x}' for i, x in enumerate(roc_auc_score(y_true, y_score, multi_class='ovr', average=None, labels=list(cell_labels)))]:
+        print(i)
+
+def main():
+    parser = argparse.ArgumentParser(description="""
+                                    Calculates metrics for celesta output evaluation.
+                                    """)
+    parser.add_argument("celesta_output", type=str,
+                        help="path to merged celesta outputs")
+    args = parser.parse_args()
+
+    evaluate_celesta_output(args.celesta_output)    
+
+if __name__ == "__main__":
+    main()
